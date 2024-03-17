694cb31dbd105ac13391f4c184ba7b616a07359b
GJ Tiquia <gershom.tiquia@gmail.com>
2024-03-17 12:09:28 +0800

    feat: click to change state

--- src/game/GameView.js
+++ src/game/GameView.js
@@ -5,24 +5,24 @@ const { div, p } = van.tags;
 export function GameView() {
     return (
         div(
-            { class: "w-full max-w-96 aspect-square grid grid-flow-row divide-y-4 divide-slate-500/30 border-4 border-slate-500/30 rounded-lg" },
+            { class: "w-full max-w-96 aspect-square grid grid-rows-3 divide-y-4 divide-slate-500/30 border-4 border-slate-500/30 rounded-lg" },
 
             div(
-                { class: "grid grid-flow-col divide-x-4 divide-slate-500/30" },
+                { class: "grid grid-cols-3 divide-x-4 divide-slate-500/30" },
                 Cell,
                 Cell,
                 Cell,
             ),
 
             div(
-                { class: "grid grid-flow-col divide-x-4 divide-slate-500/30" },
+                { class: "grid grid-cols-3 divide-x-4 divide-slate-500/30" },
                 Cell,
                 Cell,
                 Cell,
             ),
 
             div(
-                { class: "grid grid-flow-col divide-x-4 divide-slate-500/30" },
+                { class: "grid grid-cols-3 divide-x-4 divide-slate-500/30" },
                 Cell,
                 Cell,
                 Cell,
@@ -32,7 +32,25 @@ export function GameView() {
 }
 
 function Cell() {
+
+    const cellState = van.state(CELL_STATE.EMPTY);
+
     return (
-        div({ class: "flex justify-center items-center text-slate-50 text-6xl sm:text-7xl" }, "O")
+        div({
+            class: `
+                flex justify-center items-center 
+                text-slate-50 text-6xl sm:text-7xl
+                hover:bg-slate-600/50 active:bg-slate-800/50
+            `,
+            onclick: () => cellState.val = CELL_STATE.X
+        },
+            cellState
+        )
     )
+}
+
+const CELL_STATE = {
+    EMPTY: "",
+    X: "X",
+    O: "O"
 }
